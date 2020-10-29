f1d9fd5b47de342473b3a2f04f448df6246853c8
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-10-29 16:04:47 +0100

    Updated Register
    If you click the register button, you get to the Login page

--- Project/app/src/main/java/ch/noseryoung/invist/RegisterActivity.java
+++ Project/app/src/main/java/ch/noseryoung/invist/RegisterActivity.java
@@ -2,7 +2,10 @@ package ch.noseryoung.invist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    private void openLoginActivity(){
+        Intent loginActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(loginActivityIntent);
     }
 }
