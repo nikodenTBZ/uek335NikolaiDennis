f6d931c93224eb6611441e3468f35a31544b0579
Dennis Miceli <dennis.miceli@hotmail.ch>
2020-10-29 13:31:22 +0100

    Added links to the register and home activities

--- Project/app/src/main/java/ch/noseryoung/invist/MainActivity.java
+++ Project/app/src/main/java/ch/noseryoung/invist/MainActivity.java
@@ -2,8 +2,11 @@ package ch.noseryoung.invist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonLogIn = findViewById(R.id.buttonLogIn);
+
+        TextView textViewRegister = findViewById(R.id.textViewRegisterLink);
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+
+        buttonLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeActivity();
+            }
+        });
     }
+
+    /**
+     * Changes to the HomeActivity
+     */
+    private void openHomeActivity(){
+            //Change to Home-Activity
+            Intent homeActivityIntent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(homeActivityIntent);
+        }
+
+    /**
+     * Changes to the RegisterActivity
+     */
+    private void openRegisterActivity(){
+
+        Intent registerActivityIntent = new Intent(getApplicationContext(),RegisterActivity.class);
+        startActivity(registerActivityIntent);
+        }
+
+
+
+
 }
