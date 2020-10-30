dbad31e60dcf9df62be7ded4648a5befa95dbf21
Dennis Miceli <dennis.miceli@hotmail.ch>
2020-10-30 13:58:10 +0100

    Removed the Action Bar from the Main and Register Activity

--- Project/app/src/main/java/ch/noseryoung/invist/MainActivity.java
+++ Project/app/src/main/java/ch/noseryoung/invist/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Remove Action Bar
+        getSupportActionBar().hide();
+
 
         Button buttonLogIn = findViewById(R.id.buttonLogIn);
 
@@ -37,22 +43,20 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Changes to the HomeActivity
      */
-    private void openHomeActivity(){
-            //Change to Home-Activity
-            Intent homeActivityIntent = new Intent(getApplicationContext(), HomeActivity.class);
-            startActivity(homeActivityIntent);
-        }
+    private void openHomeActivity() {
+        //Change to Home-Activity
+        Intent homeActivityIntent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(homeActivityIntent);
+    }
 
     /**
      * Changes to the RegisterActivity
      */
-    private void openRegisterActivity(){
+    private void openRegisterActivity() {
 
-        Intent registerActivityIntent = new Intent(getApplicationContext(),RegisterActivity.class);
+        Intent registerActivityIntent = new Intent(getApplicationContext(), RegisterActivity.class);
         startActivity(registerActivityIntent);
-        }
-
-
+    }
 
 
 }
