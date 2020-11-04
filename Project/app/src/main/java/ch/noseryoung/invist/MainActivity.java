9474d35e968b2ccfe300553f67f950f601b6d81d
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-11-04 15:38:07 +0100

    Updated Dummy

--- Project/app/src/main/java/ch/noseryoung/invist/MainActivity.java
+++ Project/app/src/main/java/ch/noseryoung/invist/MainActivity.java
@@ -50,38 +50,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         insertDummy("dennis.miceli@hotmail.ch");
-
+        insertDummy("nikolai.schunk@gmail.com");
 
     }
 
-    public boolean checkIfEmailisUnique(String email){
-
+    public boolean checkIfEmailisUnique(String email) {
         return userDao.getUser(email) == null;
     }
 
-    public void insertDummy(String email){
+    public void insertDummy(String email) {
+        if (checkIfEmailisUnique(email)) {
 
-        if (checkIfEmailisUnique(email)){
-            User user = new User("dennis.miceli@hotmail.ch");
-            Log.d(TAG,"Created new User");
+            //================================================//
+            User userDennis = new User("dennis.miceli@hotmail.ch");
+            Log.d(TAG, "Created new User");
+            //================================================//
+            userDennis.setFirstname("Dennis");
+            userDennis.setLastname("Miceli");
+            Log.d(TAG, "Added First and Lastname to User");
+            //================================================//
+            userDennis.setBirthday(new Date());
+            Log.d(TAG, "Added Date to User");
+            //================================================//
 
-            user.setFirstname("Dennis");
-            user.setLastname("Miceli");
-            Log.d(TAG,"Added name and firstname to User");
-            user.setBirthday(new Date());
-            Log.d(TAG,"Added Date to User");
-
-            userDao.insertUser(user);
-            Log.d(TAG,"Insert User to DB");
+            //================================================//
+            User userNikolai = new User("nikolai.schunk@gmail.com");
+            Log.d(TAG, "Created new User");
+            //================================================//
+            userNikolai.setFirstname("Nikolai");
+            userNikolai.setLastname("Schunk");
+            Log.d(TAG, "Added First and Lastname to user");
+            //================================================//
+            userNikolai.setBirthday(new Date(2003, 11, 29));
+            Log.d(TAG, "Added Date to User");
+            //================================================//
+            userNikolai.setPassword("ABC123");
+            Log.d(TAG, "Added Passwort (In String)");
+            //================================================//
+            userNikolai.setCompany("Accenture");
+            Log.d(TAG, "Added Company");
+            //================================================//
+            userNikolai.setPhoneNumber("078 406 73 65");
+            Log.d(TAG, "Added Phone Number");
+            //================================================//
+            userNikolai.setAddress("Hohenbühlstrasse 8");
+            Log.d(TAG, "Added Adress");
+            //================================================//
+            userNikolai.setCity("Zürich");
+            Log.d(TAG, "Added City");
+            //================================================//
+            userNikolai.setPostcode(8032);
+            Log.d(TAG, "Added PostCode");
+            //================================================//
+            userDao.insertUser(userDennis);
+            userDao.insertUser(userNikolai);
+            Log.d(TAG, "Insert User to DB");
+            //================================================//
+            getAllUserFromDB();
         }
+    }
 
-
+    private void getAllUserFromDB() {
         List<User> userList = userDao.getAll();
-        Log.d(TAG,"Get all Users from DB");
+        Log.d(TAG, "Get all Users from DB");
 
-        for (User userTemp : userList){
+        for (User userTemp : userList) {
             System.out.println(userTemp.getEmail());
         }
     }
@@ -90,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
      * Changes to the HomeActivity
      */
     private void openHomeActivity() {
-        Log.i(TAG,"Open the HomeActivity");
+        Log.i(TAG, "Open the HomeActivity");
         //Change to Home-Activity
         Intent homeActivityIntent = new Intent(getApplicationContext(), HomeActivity.class);
         startActivity(homeActivityIntent);
@@ -100,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
      * Changes to the RegisterActivity
      */
     private void openRegisterActivity() {
-        Log.i(TAG,"Open the RegisterActivity");
+        Log.i(TAG, "Open the RegisterActivity");
         Intent registerActivityIntent = new Intent(getApplicationContext(), RegisterActivity.class);
         startActivity(registerActivityIntent);
     }
