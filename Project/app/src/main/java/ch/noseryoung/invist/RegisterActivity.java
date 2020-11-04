4dbf3bf16b20094fa4d6114c2f4cc522d01a1763
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-11-04 15:37:57 +0100

    Added a check bevor put Into DB

--- Project/app/src/main/java/ch/noseryoung/invist/RegisterActivity.java
+++ Project/app/src/main/java/ch/noseryoung/invist/RegisterActivity.java
@@ -17,15 +17,23 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+
+import org.w3c.dom.Text;
+
+import ch.noseryoung.invist.persistence.AppDatabase;
+import ch.noseryoung.invist.persistence.UserDao;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "ch.noseryoung.invist.RegisterActivity";
+    private UserDao userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG,"Create RegisterActivity");
+        Log.d(TAG, "Create RegisterActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        userDao = AppDatabase.getAppDb(getApplicationContext()).getUserDao();
         //Add a back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Register");
@@ -42,18 +50,58 @@ public class RegisterActivity extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText email = findViewById(R.id.textviewEmailRegister);
+                String sEmail = email.getText().toString();
+                if(!AreAllFieldsFilled()){
+                    TextView fieldsEmpty = findViewById(R.id.registerErrorTextView);
+                    fieldsEmpty.setText(R.string.notAllFieldsFilledError);
+                } else if (!checkIfEmailisUnique(sEmail)){
+                    TextView fieldsEmpty = findViewById(R.id.registerErrorTextView);
+                    fieldsEmpty.setText(R.string.notAllFieldsFilledError);
+                }
                 registerButtonAction();
             }
         });
 
         //TextView errorMessage = findViewById(R.id.registerErrorTextView);
         //errorMessage.setText("falscher Benutzer");
+    }
+
+    public boolean AreAllFieldsFilled() {
+        EditText firstName = findViewById(R.id.textviewFirstnameRegister);
+        EditText lastName = findViewById(R.id.textviewLastnameRegister);
+        EditText email = findViewById(R.id.textviewEmailRegister);
+        EditText password = findViewById(R.id.textviewPasswordRegister);
+        EditText birthday = findViewById(R.id.textviewBirthdayRegister);
+        Spinner company = findViewById(R.id.spinnerCompanyRegister);
+        EditText phoneNumber = findViewById(R.id.textviewPhoneNumberRegister);
+        EditText adress = findViewById(R.id.textviewAdressRegister);
+        EditText city = findViewById(R.id.textviewCityRegister);
+        EditText postcode = findViewById(R.id.textviewPostCodeRegister);
+
+        String sFirstName = firstName.getText().toString();
+        String sLastName = lastName.getText().toString();
+        String sEmail = email.getText().toString();
+        String sPassword = password.getText().toString();
+        String sBirthday = birthday.getText().toString();
+        String sCompany = company.getSelectedItem().toString();
+        String sPhoneNumber = phoneNumber.getText().toString();
+        String sAdress = adress.getText().toString();
+        String sCity = city.getText().toString();
+        String sPostcode = postcode.getText().toString();
+
+        //checks if every Field isnt empty then return true
+        return !sFirstName.matches("") && !sLastName.matches("") && !sEmail.matches("") && !sPassword.matches("") && !sBirthday.matches("") &&
+                !sCompany.matches("") && !sPhoneNumber.matches("") && !sAdress.matches("") && !sCity.matches("") && !sPostcode.matches("");
 
     }
 
+    public boolean checkIfEmailisUnique(String email) {
+        return userDao.getUser(email) == null;
+    }
 
     private void registerButtonAction() {
-        Log.d(TAG,"Back to Login with Register button");
+        Log.d(TAG, "Back to Login with Register button");
         finish();
     }
 }
